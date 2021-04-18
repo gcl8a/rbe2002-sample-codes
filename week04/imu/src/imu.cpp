1f6586d14b1b60fa5277b313067215a3402e7a80
Greg <engineering.supergenius@gmail.com>
2021-04-18 10:39:34 -0400

    updates

--- week04/imu/src/imu.cpp
+++ week04/imu/src/imu.cpp
@@ -1,23 +1,6 @@
 // This example reads the raw values from the LSM6DS33
 // accelerometer and gyro and prints those raw values to the
 // serial monitor.
-//
-// The accelerometer readings can be converted to units of g
-// using the conversion factors specified in the "Mechanical
-// characteristics" table in the LSM6DS33 datasheet.  We use a
-// full scale (FS) setting of +/- 16 g, so the conversion factor
-// is 0.488 mg/LSB (least-significant bit).  A raw reading of
-// 2048 would correspond to 1 g.
-//
-// The gyro readings can be converted to degrees per second (dps)
-// using the "Mechanical characteristics" table in the LSM6DS33
-// datasheet.  We use a full scale (FS) of +/- 1000 dps so the
-// conversion factor is 35 mdps/LSB.  A raw reading of 2571
-// would correspond to 90 dps.
-//
-// To run this sketch, you will need to install the LSM6 library:
-//
-// https://github.com/pololu/lsm6-arduino
 
 #include <Romi32U4.h>
 
@@ -45,24 +28,17 @@ void setup()
 
   imu.enableDefault();
 
-  // Set the gyro full scale to 1000 dps because the default
-  // value is too low, and leave the other settings the same.
-  imu.setFullScaleGyro(LSM6::GYRO_FS1000);
-  imu.setGyroDataOutputRate(LSM6::GYRO_ODR52);
+  // Set the gyro full scale and data rate
+  // imu.setFullScaleGyro(LSM6::GYRO_FS500);
+  imu.setGyroDataOutputRate(LSM6::ODR13);
 
-  // Set the accelerometer full scale to 16 g because the default
-  // value is too low, and leave the other settings the same.
-  imu.setFullScaleAcc(LSM6::ACC_FS16);
-
-  while(!Serial){}
-  Serial.println(imu.readReg(LSM6::CTRL1_XL), HEX);
-  while(!buttonB.getSingleDebouncedPress()){}
+  // Set the accelerometer full scale and data rate
+  // imu.setFullScaleAcc(LSM6::ACC_FS4);
+  imu.setAccDataOutputRate(LSM6::ODR13);
 }
-
 
 bool showAcc = true;
 bool showGyro = false;
-
 
 void loop()
 {
@@ -72,7 +48,7 @@ void loop()
   if(buttonB.getSingleDebouncedPress()) showAcc = !showAcc;
   if(buttonC.getSingleDebouncedPress()) showGyro = !showGyro;
 
-  if(imu.getStatus() & 0x02)
+  if(imu.getStatus() & 0x01)
   {
     imu.read();
 
